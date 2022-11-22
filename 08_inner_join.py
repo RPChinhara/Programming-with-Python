8cc67978c14d3a0f0c7931420d4edfb965fc252f
RPChinhara <rpchinhara@gmail.com>
2022-11-22 19:05:31 +0530

    lab 8 to 11

--- 08_inner_join.py
+++ 08_inner_join.py
@@ -28,13 +28,13 @@ x1=Student(1,20,'A','CSE')   # sid, age, name, dept_name
 x2=Student(2,20,'B','EE')
 x3=Student(3,20,'C','CIVIL')
 x4=Student(4,20,'D','CSE')
-    
+
 # OBJECT OF DEPARTMENT CLASS
 y1=Department(10,2,'EE')     # dept_id, sid, name
 y2=Department(11,3,'CIVIL')
 y3=Department(12,4,'CSE')
 y4=Department(13,1,'CSE')
-           
-Student=[x1,x2,x3,x4]
-Department=[y1,y2,y3,y4]
+
+Student=[x1,x2,x3,x4]  # type: ignore
+Department=[y1,y2,y3,y4]  # type: ignore
 print(inner_join(Student,Department))    
