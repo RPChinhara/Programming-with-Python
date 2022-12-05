c3ce51ec586115fa0fd0ff97bee7731455603a3d
RPChinhara <rpchinhara@gmail.com>
2022-12-05 15:39:57 +0530

    lab 2

--- 02_string_permutations.py
+++ 02_string_permutations.py
@@ -1,5 +1,5 @@
 # Python program to print all permutations of a given string of length 3 
-  
+
 def toString(List): 
     return ''.join(List) 
 
@@ -12,6 +12,6 @@ def permute(a, l, r):
             a[l], a[i] = a[i], a[l] 
             permute(a, l+1, r) 
             a[l], a[i] = a[i], a[l] 
-  
+
 string = "abc"
 permute(list(string), 0, len(string))
